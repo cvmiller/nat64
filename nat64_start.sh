52483048cde3403e907a40348463acc0a692f6b2
Craig Miller <cvmiller@gmail.com>
2019-01-14 11:46:25 -0800

    version 1.0 - updated to handle non-GUA WAN interfaces

--- nat64_start.sh
+++ nat64_start.sh
@@ -32,7 +32,7 @@ NAT64_PREFIX="64:ff9b::/96"
 #NAT64_PREFIX="2001:470:ebbd:ff9b::/96"
 
 # script version
-VERSION=0.99
+VERSION=1.0
 
 
 usage () {
@@ -47,12 +47,6 @@ usage () {
 	exit 1
 }
 
-
-# check that the WAN interface has been passed in
-#if [ $# -eq 0 ]; then
-#	usage
-#	exit 1
-#fi
 
 
 # default options values
@@ -127,19 +121,23 @@ echo "=== WAN6 $WAN_IP6"
 echo "=== LAN6 $LAN_IP6"
 echo "=== NAT64 Prefix $NAT64_PREFIX"
 
+# test no GUA on WAN
+#WAN_IP6=""
+
+
 # check that the addresses have been collected
 
 if [ "$LAN_IP6" == "" ]; then
-	echo "LAN GUA IPv6 not detected. NAT64 requires end to end IPv6 connectivity"
+	echo "ERROR: LAN GUA IPv6 not detected. NAT64 requires end to end IPv6 connectivity"
 	exit 1
 fi
 
 if [ "$WAN_IP6" == "" ]; then
-	echo "WAN GUA IPv6 not detected. NAT64 requires end to end IPv6 connectivity"
-	exit 1
+	echo "WARNING: WAN GUA IPv6 not detected. NAT64 requires end to end IPv6 connectivity, NAT64 may not work properly"
+	#exit 1
 fi
 if [ "$WAN_IP4" == "" ]; then
-	echo "WAN GUA IPv4 not detected. NAT64 requires WAN IPv4 connectivity"
+	echo "ERROR: WAN GUA IPv4 not detected. NAT64 requires WAN IPv4 connectivity"
 	exit 1
 fi
 
@@ -155,10 +153,11 @@ ip6tables -D forwarding_rule -o nat64 -j ACCEPT
 killall tayga
 
 # make tayga db dir
-mkdir -p /tmp/db/tayga
+msg=$(mkdir -p /tmp/db/tayga 2>&1)
 
 # create tayga config file
-mv $tayga_conf $tayga_conf.old
+msg=$(mv $tayga_conf $tayga_conf.old 2>&1)
+
 touch $tayga_conf
 echo "tun-device nat64" >> $tayga_conf
 echo "ipv4-addr 192.168.2.1 " >> $tayga_conf	 
@@ -179,7 +178,7 @@ ip link set nat64 up
 rm /tmp/db/tayga/dynamic.map
 
 ip addr add "$WAN_IP4" dev nat64	   		
-ip addr add "$WAN_IP6" dev nat64
+#ip addr add "$WAN_IP6" dev nat64
 #ip addr add "192.168.2.1" dev nat64	   		
 #ip addr add $LAN_IP6 dev nat64
 ip route add 192.168.2.0/24 dev nat64
@@ -198,6 +197,7 @@ NAT64_FRONT=$(echo $NAT64_PREFIX | cut -d '/' -f 1)
 ping6 -c3 $NAT64_FRONT"8.8.4.4"
 
 echo "Pau!"
+
 
 
 
